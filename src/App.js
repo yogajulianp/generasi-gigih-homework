c702c779322ec0841b5083bed9175fc657a8b875
Yoga Julian Prasutiyo <yogajprasutiyo@gmail.com>
2021-07-12 11:43:11 +0700

    back App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
+
 
 function App() {
   return (
@@ -23,15 +23,6 @@ function App() {
   );
 }
 
-componentDidMount() {
-  axios.get('https://developer.spotify.com/dashboard/applications/eabe295c841b4c44a8948329648bdc78')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-}
 
 
 export default App;
